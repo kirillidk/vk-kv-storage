e452181dc6869e260ac25277c3b35f64d5edf446
kirillidk <k.mironov03@gmail.com>
2025-08-17 11:42:42 +0300

    build: Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -7,3 +7,37 @@ class KVStorage(ConanFile):
     def requirements(self):
         self.requires("boost/1.88.0")
         self.requires("gtest/1.16.0")
+    
+    def configure(self):
+        self.options["boost"].without_atomic = False
+        self.options["boost"].without_chrono = False
+        self.options["boost"].without_container = False
+        self.options["boost"].without_context = False
+        self.options["boost"].without_date_time = False
+        self.options["boost"].without_exception = False
+        self.options["boost"].without_filesystem = False
+        self.options["boost"].without_system = False
+        self.options["boost"].without_thread = False
+
+        self.options["boost"].without_coroutine = True
+        self.options["boost"].without_fiber = True
+        self.options["boost"].without_graph = True
+        self.options["boost"].without_graph_parallel = True
+        self.options["boost"].without_iostreams = True
+        self.options["boost"].without_json = True
+        self.options["boost"].without_locale = True
+        self.options["boost"].without_log = True
+        self.options["boost"].without_math = True
+        self.options["boost"].without_mpi = True
+        self.options["boost"].without_nowide = True
+        self.options["boost"].without_program_options = True
+        self.options["boost"].without_python = True
+        self.options["boost"].without_random = True
+        self.options["boost"].without_regex = True
+        self.options["boost"].without_serialization = True
+        self.options["boost"].without_stacktrace = True
+        self.options["boost"].without_test = True
+        self.options["boost"].without_timer = True
+        self.options["boost"].without_type_erasure = True
+        self.options["boost"].without_url = True
+        self.options["boost"].without_wave = True
